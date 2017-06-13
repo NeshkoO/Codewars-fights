55342bfbb0b6fdcbe7d43b5da63e315420750c49
Alex Orlowskyy <rpgooyea@gmail.com>
2017-06-14 00:10:30 +0300

    "Two fighters, one winner" task reorganized

--- 7 kyu/two_fighters_one_winner.py
+++ 7 kyu/two_fighters_one_winner.py
@@ -1,4 +1,15 @@
 __author__ = 'Alex Orlovskyi'
+
+'''
+"Two fighters, one winner" task, provided by codewars.com/users/MirzaI . 7 kyu difficulty level.
+
+Create a function that returns the name of the winner in a fight between two fighters.
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+'''
+
+
 class Fighter(object):
     def __init__(self, name, health, damage_per_attack):
         self.name = name
@@ -17,10 +28,10 @@ def declare_winner(fighter1, fighter2, first_attacker):
     return fighter2.name
 
 
-
 f1 = Fighter("Jerry", 20, 5)
 f2 = Fighter("Harald", 30, 3)
 
+#some self-testing before send
 print(declare_winner(f1,f2,"Harald"))
 print(30//5 + (30%5>0))
 print(20//3 + (20%3>0))
